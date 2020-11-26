47e5e1d5191314688d279fd2acacf303bb45d0fd
Brian Smith <hoshiningen@gmail.com>
2020-11-26 11:43:30 -0500

    Replaced explicit validation in the script with JSON schema validation

--- generate.py
+++ generate.py
@@ -3,6 +3,11 @@ import json
 import os
 import random
 
+from jsonschema import validate
+
+
+DB_SCHEMA = r"./database.json"
+
 
 class WordDatabase(object):
     """Represents a word database"""
@@ -11,8 +16,16 @@ class WordDatabase(object):
         if not os.path.isfile(database_file):
             raise argparse.ArgumentTypeError(f"Unabled to load: '{database_file}'!")
 
+        database_schema = {}
+        with open(DB_SCHEMA, 'r') as schema:
+            database_schema = json.load(schema)
+
         with open(database_file, 'r') as db:
             self.__dict__ = json.load(db)
+
+        # Ensure the database is well-formed
+        validate(self.__dict__, database_schema)
+
 
     def __str__(self):
         return self.__dict__["name"]
@@ -22,12 +35,13 @@ class WordDatabase(object):
 
 
 def word_count_validator(word_count):
-    """A validator to ensure the given word count is in the valid range [4, inf)"""
+    """A validator to ensure the given word count is in the valid range"""
 
     count = int(word_count)
 
     if count < 4:
-        raise argparse.ArgumentTypeError(f"The valid range of word counts is [4, inf)!")
+        raise argparse.ArgumentTypeError(f"The chosen word count '{count}' is smaller than the "
+                                         f"minimum required (4).")
 
     return count
 
@@ -66,20 +80,8 @@ def main():
     parser = setup_arguments()
     args = parser.parse_args()
 
-    # Remove any duplicate words from the database
-    database_words = list(set(args.database[0].words))
-
-    # Verify that the selected word count isn't greater than the number of words in the database
-    if args.word_count and len(database_words) < args.word_count:
-        raise Exception(f"The number of words you're asking for is greater "
-                        f"than the amount contained in the given database! "
-                        f"(Desired: {args.word_count}, Given: {len(database_words)})")
-    
-    # Verify that the length of all words in the database are of acceptable length
-    if any(map(lambda word: len(word) > 30, database_words)):
-        raise Exception("A word in the given database is larger than 30 characters!")
-
-    chosen_words = choose_words(database_words, args.word_count)
+    # Do the selection thing
+    chosen_words = choose_words(args.database[0].words, args.word_count)
     print(', '.join(chosen_words))
 
 
